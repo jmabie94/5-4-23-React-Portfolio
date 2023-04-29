a1f39b0e338834c340781394fa7cefeb56cc7f74
Jack Mabie <johnmabie94@gmail.com>
2023-04-28 20:35:09 -0500

    starting on components

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+/* might need to include an About page and more components, unsure as of now */
+import Home from './pages/Home';
+import AllProjects from './pages/AllProjects';
+import Contact from './pages/Contact';
+
+function App() {
+    return (
+        <Router>
+            <div className='App'>
+                <Header />
+                <div className='container'>
+                    <Routes>
+                        <Route path='/' element={<Home />}/>
+                        <Route path='/projects' element={<AllProjects />}/>
+                        <Route path='/contact' element={<Contact />}/>
+                    </Routes>
+                </div>
+                <Footer />
+            </div>
+        </Router>
+    );
+}
+
+export default App;

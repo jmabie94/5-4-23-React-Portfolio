c750b21c306f8b9b721cbc3df44616889a5a3c6d
Jack Mabie <johnmabie94@gmail.com>
2023-04-28 23:33:24 -0500

    starting basic pages structure, adding images, creating global css

---
+++ src/pages/AllProjects.js
@@ -0,0 +1,5 @@
+import SingleProject from "./SingleProject";
+import { useEffect, useState } from "react";
+/* building AllProjects by importing multiple different individual SingleProjects */
+
+export default AllProjects;
---
+++ src/pages/Contact.js
@@ -0,0 +1,4 @@
+import React from "react";
+import './Contact.css';
+
+export default ContactForm;

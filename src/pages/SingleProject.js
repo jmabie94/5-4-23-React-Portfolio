42ec0e100cbd263fa51eb73795a145e3d38a4c66
Jack Mabie <johnmabie94@gmail.com>
2023-05-05 18:25:59 -0500

    struggling to match styling between project and home pages

--- src/pages/SingleProject.js
+++ src/pages/SingleProject.js
@@ -11,50 +11,50 @@ const SingleProject = ({ projects, title }) => {
 
     return (
         <>
-            <Grid container spacing={2}>
+            <Grid container spacing={2} margin={0}>
                 <div className="project-header">
-                    <Grid xs ={12}>
+                    <Grid xs ={12} direction={"row"}>
                             <div className="page-title">{title}</div>
                     </Grid>
-                    <Grid xs ={12}>
+                    <Grid xs ={12} direction={"row"}>
                             <div className="contact-bar">
                                 <span className="phone">+1 (626) 864-0110</span>
                                 <span className="email">johnmabie94@gmail.com</span>
                                 <span className="city">Tulsa, Oklahoma</span>
                             </div>
-                    </Grid>
-                    <Grid xs={12}>
+                    </Grid>{/* 
+                    <Grid xs={12} direction={"row"}>
                             <div className="project-intro">
                                 <div className="text-box text-project">
                                     <div id="box5">
-                                        <h2>Here are some projects I have worked on, either solo or as part of a group:</h2>
+                                        <p>Here are some projects I have worked on, either solo or as part of a group:</p>
                                     </div>
                                 </div>
                             </div>
-                    </Grid>
+                    </Grid> */}
                 </div>
                 <div className="project-grid">
                     {projects.map((project) => {
                         return (
                             <Grid xs={6}>
-                                    <Card sx={{ maxWidth: 600 }}>
-                                        <CardMedia
-                                            sx={{ height: 200 }}
-                                            image={ project.screenshot }
-                                            title={ project.title }/>
-                                        <CardContent>
-                                            <Typography variant="h5" component="div">
-                                                { project.title }
-                                            </Typography>
-                                            <Typography variant="body2" color="text.secondary">
-                                                { project.description }
-                                            </Typography>
-                                        </CardContent>
-                                        <CardActions>
-                                            <Button href={project.url} size="small" startIcon={<Avatar src={viewIcon}/>}>View</Button>
-                                            <Button href={project.github} size="small" startIcon={<Avatar src={gitIcon}/>}>GitHub</Button>
-                                        </CardActions>
-                                    </Card>
+                                <Card sx={{ maxWidth: 1000 }}>
+                                    <CardMedia
+                                        sx={{ height: 200 }}
+                                        image={ project.screenshot }
+                                        title={ project.title }/>
+                                    <CardContent>
+                                        <Typography variant="h5" component="div">
+                                            { project.title }
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            { project.description }
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button href={project.url} size="small" startIcon={<Avatar src={viewIcon}/>}>View</Button>
+                                        <Button href={project.github} size="small" startIcon={<Avatar src={gitIcon}/>}>GitHub</Button>
+                                    </CardActions>
+                                </Card>
                             </Grid>
                         );
                     })}
@@ -63,5 +63,7 @@ const SingleProject = ({ projects, title }) => {
         </>
     );
 };
+
+// 
 
 export default SingleProject;
